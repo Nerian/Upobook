27ab0e7be9332770d34bd078dc3499a06d90fb70
Nerian <siotopo@gmail.com>
2010-06-26 16:51:28 +0200

    Validation in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+  validates :name, :password, :mail, :presence => true;
+  validates :mail, :uniqueness => true;
+  validates :mail, :format => {:with => /^[a-z]*@[a-z]*.com$/i,
+    :message => "You must insert an email in the form example@example.com"
+  }
+
 end
