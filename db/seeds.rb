dfe0ac475f790dd817f4fd7b294cbab8dd694e10
Nerian <siotopo@gmail.com>
2010-06-26 16:42:42 +0200

    add some seeds for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.delete_all
+
+User.create(:name => "Gonzalo", :password => "12345", :mail=>"gonzalo@upo.es")
+User.create(:name => "Godofredo", :password => "12345", :mail=>"Godofredo@upo.es")
+User.create(:name => "Altair", :password => "12345", :mail=>"Altair@upo.es")
+User.create(:name => "Nicolas", :password => "12345", :mail=>"Nicolas@upo.es")
+User.create(:name => "Pepito", :password => "12345", :mail=>"Pepito@upo.es")
+User.create(:name => "Sauron", :password => "12345", :mail=>"Sauron@upo.es")
