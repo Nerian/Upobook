54e79a7d11c3644ec855f95b2c192a6b62eca8cf
Nerian <siotopo@gmail.com>
2010-06-28 21:51:04 +0200

    Añado seed para Channel, Channel_User y Coment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 User.delete_all
-Channel.delete_all
 
 user1 = User.create(:name => "Gonzalo", :password => "12345", :mail=>"gonzalo@upo.com")
 user2 = User.create(:name => "Godofredo", :password => "12345", :mail=>"Godofredo@upo.com")
@@ -16,6 +15,27 @@ user4 = User.create(:name => "Nicolas", :password => "12345", :mail=>"Nicolas@up
 user5 = User.create(:name => "Pepito", :password => "12345", :mail=>"Pepito@upo.com")
 user6 = User.create(:name => "Sauron", :password => "12345", :mail=>"Sauron@upo.com")
 
-Channel.create(:name => "Soporte", :description => "Información para el usuario", :owner=> user6.id)
-Channel.create(:name => "Historia", :description => "Chat sobre la historia de europa", :owner => user4.id)
+Channel.delete_all
+
+chanel1 = Channel.create(:name => "Soporte", :description => "Información para el usuario", :owner=> user6.id)
+chanel2 = Channel.create(:name => "Historia", :description => "Chat sobre la historia de europa", :owner => user4.id)
+
+ChannelUser.delete_all
+
+ChannelUser.create(:channel_id => chanel1.id, :user_id => user1.id)
+ChannelUser.create(:channel_id => chanel1.id, :user_id => user2.id)
+ChannelUser.create(:channel_id => chanel1.id, :user_id => user3.id)
+ChannelUser.create(:channel_id => chanel1.id, :user_id => user4.id)
+ChannelUser.create(:channel_id => chanel2.id, :user_id => user5.id)
+ChannelUser.create(:channel_id => chanel2.id, :user_id => user6.id)
+
+Comment.delete_all
+
+Comment.create(:channel_id => chanel1.id, :user_id => user1.id, :message => "¿Lo tienes encendido?.")
+Comment.create(:channel_id => chanel1.id, :user_id => user2.id, :message => "Sí.")
+Comment.create(:channel_id => chanel1.id, :user_id => user1.id, :message => "¿Has probado a reiniciar?.")
+Comment.create(:channel_id => chanel1.id, :user_id => user2.id, :message => "No.")
+Comment.create(:channel_id => chanel1.id, :user_id => user1.id, :message => "Pruebalo y me cuentas.")
+Comment.create(:channel_id => chanel2.id, :user_id => user3.id, :message => "Si, la hermandad pronto se reunirá")
+Comment.create(:channel_id => chanel2.id, :user_id => user4.id, :message => "Dios lo dispone.")
 
