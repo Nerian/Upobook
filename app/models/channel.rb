b85a2daedeb1d110cd93ef198734c7b9b7d93603
Nerian <siotopo@gmail.com>
2010-06-30 12:55:50 +0200

    Refactoring de Channel. Ahora tiene un campo Users con todos los usuarios en el canal.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,7 @@
 class Channel < ActiveRecord::Base
 
   has_many :channel_users, :dependent => :destroy
+  has_many :users, :through => :channel_users
   belongs_to :user, :foreign_key => "owner"
 
   validates :name, :description, :owner, :presence => true;
