3b22fcd75452380dea52ac961e03f788fa4d4102
Nerian <siotopo@gmail.com>
2010-06-30 16:24:58 +0200

    Los canales se muestran ordenados alfabeticamente

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,7 +1,7 @@
 class ChatController < ApplicationController  
 
   def index
-    @channels = Channel.all
+    @channels = Channel.order("name ASC")
     if id = session[:channel]
       @channel = Channel.find(id)
       @comment = Comment.new
