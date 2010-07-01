43c070eea068e190ba64a18a5a4461dbcd9032cd
Nerian <siotopo@gmail.com>
2010-07-01 14:34:43 +0200

    Fix, el email ahora acepta números

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
  
   validates :name, :password, :mail, :presence => true;
   validates :mail, :uniqueness => true;
-  validates :mail, :format => {:with => /^[a-z]*@[a-z]*.com$/i,
-    :message => "You must insert an email in the form 'example@example.com'"
+  validates :mail, :format => {:with => /^\w*@\w*.\w*$/i,
+    :message => "Dirección de mail inválida"
   }
 
   class <<self
